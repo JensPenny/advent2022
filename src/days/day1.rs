0bd6e651c369f049b7f9b247333cc1c886fa2bed
jens <jens.penny@gmail.com>
2022-12-01 18:39:05 +0100

    perfecte zelf gevonden oplossing voor d1b

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,23 +1,29 @@
 use std::num::ParseIntError;
 
-pub fn day_1_a(input: &String) -> u32 {
+pub fn day_1_a(input: &String, length: usize) -> Vec<u32> {
     let result: Vec<String> = input.lines().map(|s| s.to_string()).collect();
 
-    let mut max: u32 = 0;
+    let mut maxes: Vec<u32> = Vec::with_capacity(length);
     let mut current: u32 = 0;
     for line in result {
         let calories: Result<u32, ParseIntError> = line.trim().parse();
         match calories {
             Ok(num) => current += num,
             Err(_) => {
-                if current > max {
-                    max = current;
+                if maxes.len() < length {
+                    maxes.push(current);
+                } else {
+                    //todo: fix met indices nerd
+                    let min = maxes.iter_mut().min().expect("derp");
+                    if current > *min {
+                        *min = current;
+                    }
                 }
                 current = 0;
             },
         }
     }
-    max
+    maxes.iter().map(|f| *f).collect()
 }
 
 #[cfg(test)]
@@ -40,9 +46,32 @@ mod tests {
         9000
         
         10000
-        ".to_string());
-        assert_eq!(result, 24000);
-
+        ".to_string(), 1);
+        assert_eq!(result[0], 24000);
     }
+
+    #[test]
+    fn test_day_b() {
+        let mut result = day_1_a(&"1000
+        2000
+        3000
+        
+        4000
+        
+        5000
+        6000
+        
+        7000
+        8000
+        9000
+        
+        10000
+        ".to_string(), 3);
+        result.sort();
+        assert_eq!(result[0], 10000);
+        assert_eq!(result[1], 11000);
+        assert_eq!(result[2], 24000);
+    }
+
 
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ pub mod days;
 
 fn main() {
     let day1input = fs::read_to_string("res/day1").expect("Could not read file :(");
-    let day1a = day1::day_1_a(&day1input);
-    println!("Day 1A: {day1a}")
+    let day1a = day1::day_1_a(&day1input, 1)[0];
+    println!("Day 1A: {day1a}");
+    let day1b:u32 = day1::day_1_a(&day1input, 3).iter().sum();
+    println!("Day 1B: {day1b}");
 }
