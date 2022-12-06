1b0d63a8a6c46739f2b31824fe9e323ac07fa98b
jens <jens.penny@gmail.com>
2022-12-06 20:52:30 +0100

    added non-itertools solution

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 
 
@@ -13,9 +15,23 @@ pub fn day_6(input: &String, slice_size: usize) -> usize {
     return 0;
 }
 
+pub fn day_6_no_iter(input: &String, slice_size: usize) -> usize {
+
+    for i in slice_size..input.len() + 1 {
+        let slice_dice = &input[i-slice_size..i];
+        
+        let set:HashSet<&u8> = HashSet::from_iter(slice_dice.as_bytes().iter());
+        if set.len() == slice_size {
+            return i
+        }
+        //println!("sliced {slice_dice}");
+    };
+    return 0;
+}
+
 #[cfg(test)]
 mod tests {
-    use super::day_6;
+    use super::{day_6, day_6_no_iter};
 
 
     fn input1() -> String {
@@ -33,6 +49,13 @@ mod tests {
     fn test_day_6_b() {
         let input = input1();
         let result = day_6(&input, 14);
+        assert_eq!(result, 19)
+    }
+
+    #[test]
+    fn test_day_6_b_no_iter() {
+        let input = input1();
+        let result = day_6_no_iter(&input, 14);
         assert_eq!(result, 19)
     }
 
